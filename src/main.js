1122be2e2ca28bc46ac0db1d4aa2e5f6707bce8e
johannes <jmuljana@gmail.com>
2023-01-05 22:21:37 +1100

    added spriteSheets

--- src/main.js
+++ src/main.js
@@ -1,96 +1,30 @@
-// game assets
-const playerSpriteSheetImg = "/images/sprites/player/TestPlayer.png"
-const groundSpriteImg = "/images/sprites/world/Ground.png"
-const canvasID = "game"
-
-// game variables and states
-let score = 0;
-let isAlive = true
-let mouseDown = false
-let gamePaused = false
-// environment
-const playerScale = 0.15
-let canvasWidth = 0
-let canvasHeight = 0
-
-const timer = createjs.Ticker
-const stage = new createjs.Stage(canvasID);
-
-const getCanvasDimension = () => {
-  canvasHeight = document.getElementById(canvasID).offsetHeight
-  canvasWidth = document.getElementById(canvasID).offsetWidth
-  console.log(canvasWidth, canvasHeight)
-}
-//
-const playerSetUp = () => {
-  const data = {
-    images: [playerSpriteSheetImg],
-    frames: {
-      width: 400,
-      height: 300
-    },
-    animations: {
-      down: [0, 1],
-      up: [2, 3],
-      dead: [4]
-    }
-  };
-  var spriteSheet = new createjs.SpriteSheet(data);
-  var animation = new createjs.Sprite(spriteSheet, "down");
-  player = new createjs.Sprite(spriteSheet, animation);
-  player.x = 50
-  player.y = 0;
-  player.scaleX = playerScale
-  player.scaleY = playerScale
-  player.regX = 0;
-  player.regY = 80;
-  player.framerate = 15
-  player.name = "player";
-  stage.addChild(player);
-  stage.update();
-}
-let flying = false;
-let falling = false
-const playAnimation = ( name ) => {
-  if( name == "down" && falling == false ) {
-    gotoAndPlay("down")
-    falling = true
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'game-view',
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
   }
+};
+
+const game = new Phaser.Game(config);
+
+function preload ()
+{
+  //player
+  this.load.spritesheet('santa', 'images/sprites/player/PlayerSpriteSheet.png', {frameWidth: 600, frameHeight: 400})
+  //obstacles
+  this.load.spritesheet('objects', 'images/sprites/objects/Obstacles_Sprite_Sheet.png', {frameWidth: 600, frameHeight: 450})
+  // background
 }
 
-const onUpdate = (event) => {
-  if (gamePaused == false) {
-    // if the game is playing
-    if( mouseDown == true ) {
-      playAnimation("up")
-    }
-    else {
-
-    }
-  }
-  else {
-    // do paused stuff
-  }
-  stage.update(event)
+function create ()
+{
 }
 
-const initGame = () => {
-  gamePaused = false
-  timer.on("tick", onUpdate)
-  timer.framerate = 60
-  getCanvasDimension()
-  playerSetUp()
+function update ()
+{
 }
-
-// start game
-document.onload = initGame()
-
-window.addEventListener("keydown", (event) => {
-  //if spacebar is pressed
-  if (event.code == "Space") {
-    mouseDown = true;
-  }
-});
-window.addEventListener("keyup", (event) => {
-  mouseDown = false
-});
