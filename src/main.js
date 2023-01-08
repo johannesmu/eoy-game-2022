60ca3aa3cfc6622df64fb2eabf508c6d401c8644
johannes <jmuljana@gmail.com>
2023-01-09 09:55:34 +1100

    latest update

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
+      // gravity: { y: 800 },
       debug: false
     }
   },
@@ -21,13 +21,19 @@ const config = {
 const game = new Phaser.Game(config);
 
 // globals
-let cloudL, cloudS, player, keyPress
+let cloudL, cloudS, player, keyPress, text
+let isPlaying = true
+let tick = 0
+let score = 0
+// obstacles
+let obsInterval = 400
+let obstacles = []
 
 function preload() {
   //player
-  this.load.spritesheet('santa', 'images/sprites/player/PlayerSpriteSheet.png', { frameWidth: 600, frameHeight: 300 })
+  this.load.spritesheet('santa', 'images/sprites/player/SantaSpriteSheet.png', { frameWidth: 300, frameHeight: 123 })
   //obstacles
-  this.load.spritesheet('objects', 'images/sprites/objects/Obstacles_Sprite_Sheet.png', { frameWidth: 600, frameHeight: 450 })
+  this.load.spritesheet('objects', 'images/sprites/objects/Obstacles_Sprite_Sheet.png', { frameWidth: 480, frameHeight: 360 })
   // background
   this.load.image('cloudsL', 'images/backgrounds/Clouds_Large-Fluffy.png')
   this.load.image('cloudsS', 'images/backgrounds/Clouds_Small-fluffy.png')
@@ -37,6 +43,8 @@ function create() {
   // backgrounds
   cloudL = this.add.tileSprite(400, 300, 2400, 600, 'cloudsL')
   cloudS = this.add.tileSprite(400, 300, 2400, 600, 'cloudsS')
+  // text
+  text = this.add.text(0, 0, score, { fontFamily:'Arial, Helvetica, sans-serif', fontSize: 20, padding: 10, textShadow: "1 1 1 4px black" })
   // animations
   this.anims.create({
     key: "down",
@@ -60,9 +68,16 @@ function create() {
   // player
   player = this.physics.add.sprite(400, 50, 'santa')
   player.setCollideWorldBounds(true)
-  player.play("crash")
+  player.play("down")
 
-  //keys
+  // obstacle
+  this.anims.create({
+    key: "pipe",
+    framerate: 7,
+    frames: this.anims.generateFrameNumbers("objects", { start: 0, end: 0 }),
+    repeat: 1
+  })
+  // controls
   this.input.keyboard.on('keydown', function (event) {
     keyPress = event.key
   })
@@ -72,6 +87,68 @@ function create() {
 }
 
 function update() {
+  if( isPlaying == false ) {
+    return
+  }
+
   cloudL.tilePositionX += 1
   cloudS.tilePositionX += 0.25
+  
+  if(keyPress == " ") {
+    player.setVelocityY(-300)
+    player.play("up")
+  }
+  else {
+    player.setVelocityY(100)
+    player.play("down")
+  }
+  console.log( player.y )
+
+  tick++
+  if( tick % obsInterval == 0 ) {
+    console.log(`ticking ${tick}`)
+    createObstacle(this)
+  }
+  else if ( tick % 2000 == 0) {
+    obsInterval = 300
+  }
+  else if ( tick % 5000 == 0 ) {
+    obsInterval = 400
+  }
+  manageObstacles()
+}
+
+function createObstacle(e) {
+  lanes = [30, 240, 480]
+  let obj = e.physics.add.sprite(1300, lanes[ randGen(2) ] + 120,"objects")
+  const framenum = randGen(5)
+  obj.setFrame( framenum )
+  e.physics.add.collider( player, obj, ( player, obj ) => {
+    // console.log("hit")
+    isPlaying = false
+    player.play("crash")
+    player.setVelocityY(300)
+    obj.destroy()
+  }, null )
+  obstacles.push(obj)
+}
+
+function manageObstacles() {
+  // console.log( obstacles.length)
+  obstacles.forEach( (obst) => {
+    if( obst.x >= -200 ) {
+      obst.x -= 3
+    }
+    else {
+      obst.destroy()
+      let item = obstacles.shift()
+      item = null
+      score += 1
+      text.setText(score)
+    }
+  })
+}
+
+function randGen(limit) {
+  return Math.round(Math.random() * limit)
 }
